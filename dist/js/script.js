20753f6b7e103b8076fce79204d3fef4ffe720ca
riskiegenerous <riskiegenerous@gmail.com>
2022-02-06 14:07:32 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".small-menu", {
+	slidesPerView: 4,
+	grid: {
+	rows: 2,
+},
+spaceBetween: 35,
+pagination: {
+	el: ".swiper-pagination",
+	clickable: true,
+},
+});
+
+var swiper = new Swiper(".product-brand", {
+	slidesPerView: 4,
+	grid: {
+	rows: 1,
+},
+spaceBetween: 30,
+pagination: {
+	el: ".swiper-pagination",
+	clickable: true,
+},
+});
